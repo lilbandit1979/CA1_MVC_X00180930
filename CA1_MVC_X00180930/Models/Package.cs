b3e6a236c6d8d5299a7e98013468e49070aba1c9
lilbandit1979 <stephenobrien1979@Gmail.com>
2022-11-28 18:46:58 +0000

    --Model added

---
+++ CA1_MVC_X00180930/Models/Package.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CA1_MVC_X00180930.Models
+{
+    public class Package
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public string ClientName { get; set; } = "";
+        [Required]
+        public string ShippingAddress { get; set; } = "";
+        [Required]
+        public double Weight { get; set; }
+        [Required]
+
+        public double Length { get; set; }
+        [Required]
+        public double Width { get; set; }
+        [Required]
+        public double Height { get; set; }
+        [Required]
+
+        public ShippingCategory ShippingCategory
+        {
+            get
+            {
+                return ShippingCategory;
+            }
+            set
+            {
+                if(Density()<1)
+                {
+                    ShippingCategory = ShippingCategory.Low;
+                }
+                if(Density()<100)
+                {
+                    ShippingCategory = ShippingCategory.Medium;
+                }
+                if(Density()>=100)
+                {
+                    ShippingCategory = ShippingCategory.High;
+                }
+            }
+        }
+
+
+        public double Volume()
+        {
+            return Weight * Length*Height;
+        }
+        
+        public double Density()
+        {
+            double answer = Weight / Volume();
+            return answer;
+        }
+
+
+    }
+}
---
+++ CA1_MVC_X00180930/Models/ShippingCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace CA1_MVC_X00180930.Models
+{
+    public enum ShippingCategory
+    {
+        Low,
+        Medium,
+        High
+    }
+}
