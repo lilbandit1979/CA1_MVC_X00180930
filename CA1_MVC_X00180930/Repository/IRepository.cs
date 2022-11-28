be8a675afae4ba5b7547b63a615eb2d2b8f34b21
lilbandit1979 <stephenobrien1979@Gmail.com>
2022-11-28 19:44:58 +0000

    --interface added

---
+++ CA1_MVC_X00180930/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using CA1_MVC_X00180930.Models;
+
+namespace CA1_MVC_X00180930.Repository
+{
+    public interface IRepository
+    {
+        List<Package> AllFixtures();
+        Package GetPackage(int id);
+        void Create(Package package);
+        void EditFixture(Package package);
+    }
+}
