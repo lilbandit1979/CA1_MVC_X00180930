94568862291b5f3b653b48d4623286dc9d655eb0
lilbandit1979 <stephenobrien1979@Gmail.com>
2022-11-28 18:21:22 +0000

    Add project files.

---
+++ CA1_MVC_X00180930/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using CA1_MVC_X00180930.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CA1_MVC_X00180930.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CA1_MVC_X00180930/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CA1_MVC_X00180930.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
